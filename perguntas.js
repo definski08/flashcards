3dbee5b783bdb504f9a809a76ba4b09012dff9fe
definski08 <andre.definski@escola.pr.gov.br>
2024-10-28 10:17:27 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Academia',
+    'O que é a creatina?',
+    'É um suplemento que auxilia o ganho de massa corporal e na retenção de líquidos'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Austrália?',
+    'A capital da França é Camberra'
+)
+
+criaCartao(
+    'Academia',
+    'O que é o Whey Protein?',
+    'É uma suplementação que pode substituir uma refeição'
+)
+
+criaCartao(
+    'Geografia',
+    'Por que o Sri Lanka possui duas capitais?',
+    'Tem uma capital legislativa e outra executiva, respectivamente, Sri Jayewardenepura Kotte e Colombo'
+)
